bd82ca9cd547402e8ee1fc5bc13fcf10389bef42
Ashkrm <68059791+Ashkrm@users.noreply.github.com>
2021-02-09 20:57:09 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -90,28 +90,48 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "For which combinations of the input variables Wattmeter reading shows the maximum value?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "CT Ratio- 4:1, PT Ratio-440/220, Multiplying Factor- 4, and Load Selector Switch- 500W",                  ///// Write the option 1 inside double quotes
+        b: "CT Ratio- 2:1, PT Ratio-440/110, Multiplying Factor- 4, and Load Selector Switch- 500W",                  ///// Write the option 2 inside double quotes
+        c: "CT Ratio- 4:1, PT Ratio-440/110, Multiplying Factor- 4, and Load Selector Switch- 1000W",                  ///// Write the option 3 inside double quotes
+        d: "CT Ratio- 2:1, PT Ratio-440/220, Multiplying Factor- 2, and Load Selector Switch- 1000W"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
 
     {
-      question: "This is question 2?",  ///// Write the question inside double quotes
+      question: "What is the formula for calculating the total power consumed by load?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "VACosΦ",                  ///// Write the option 1 inside double quotes
+        b: "3VACosΦ",                  ///// Write the option 2 inside double quotes
+        c: "√3VACosΦ",                  ///// Write the option 3 inside double quotes
+        d: "4VACosΦ"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "b"                ///// Write the correct option inside double quotes
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
     									                  ///// this line
-
+    {
+      question: "What is the formula for calculating the %error?",  ///// Write the question inside double quotes
+      answers: {
+        a: "percent error = [experimental value - theoretical value] / theoretical value x 100%",                  ///// Write the option 1 inside double quotes
+        b: "percent error = [theoretical value - experimental value] / theoretical value x 100%",                  ///// Write the option 2 inside double quotes
+        c: "percent error = [theoretical value - experimental value] /experimental value x 100%",                  ///// Write the option 3 inside double quotes
+        d: "percent error =[calculated value- observed value]/ observed value x 100%"                   ///// Write the option 4 inside double quotes
+      },
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
+    },
+    
+        {
+      question: "What is the maximum value of 3-phase 2 element wattmeter reading by the simulator?",  ///// Write the question inside double quotes
+      answers: {
+        a: "62",                  ///// Write the option 1 inside double quotes
+        b: "125",                  ///// Write the option 2 inside double quotes
+        c: "150",                  ///// Write the option 3 inside double quotes
+        d: "200"                   ///// Write the option 4 inside double quotes
+      },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },
 
     /* To add more MCQ's, copy the below section, starting from open curly braces ( { )
         till closing curly braces comma ( }, )
